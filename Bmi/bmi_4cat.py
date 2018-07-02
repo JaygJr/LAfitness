cb0b51af9f2189126c2c27dc9bf4494dab28aa12
Larry Apuzzo <lawrenzzo@comcast.net>
2018-07-02 13:01:32 -0400

    Initial import of 4 LED version of bmi program.
    An earlier program was written for bmi categorization of user-entered
    height (inches) and weight (lbs), but that version only used 3 LED lights
    and there are 4 bmi categories (so one LED was used for 2 categoies).
    This version makes use of a 4th LED, so there is now 1 LED per category.

--- Bmi/bmi_4cat.py
+++ Bmi/bmi_4cat.py
@@ -1,13 +1,13 @@
 # Include the library functions to use LEDs.
-from gpiozero import LED
 from time import sleep
+from gpiozero import LED
 
-# Establish a pin connection for 3 LEDs (red 17, yellow 27, green 22 
+# Establish a pin connection for 3 LEDs (red 17, yellow 27, green 22
 #                                        and blue 23)
 # Note the numbers above come from pinout.xyz for 4 BCM pins
 # used for LEDs.
 red_led = LED(17)       # Red LED used for Obese category
-yellow_led = LED(27)    # Yellow LED used for Overweight category 
+yellow_led = LED(27)    # Yellow LED used for Overweight category
 green_led = LED(22)     # Green LED used for Normal BMI category
 blue_led = LED(23)      # Blue LED used for Underweight category
 
@@ -41,31 +41,29 @@ bmi = (kg / (meters**2))
 #   Overweight should light the YELLOW LED.
 #   Normal weight should light the GREEN LED.
 print()
-print ("Your BMI = %4.2f" %(bmi) + " kg/m2")
+print("Your BMI = %4.2f" %(bmi) + " kg/m2")
 print()
 
 # If bmi < 18.5
 if bmi < 18.5:
-    print ("Underweight")
+    print("Underweight")
     blue_led.on()
     sleep(5)
 
 #elIf  bmi >= 18.5 and bmi < 25
 elif  bmi >= 18.5 and bmi < 25:
-    print ("Normal")
+    print("Normal")
     green_led.on()
-    sleep(5) 
+    sleep(5)
 
 # If bmi >= 25 and bmi < 30
 elif bmi >= 25 and bmi < 30:
-    print ("Overweight")
+    print("Overweight")
     yellow_led.on()
     sleep(5)
 
-# If bmi >=30 
+# If bmi >=30
 if bmi >= 30:
-    print ("Obese")
+    print("Obese")
     red_led.on()
     sleep(5)
-
-
