0a398f4eeed4ee66f3136bfa961747061240c1a5
Larry Apuzzo <lawrenzzo@comcast.net>
2018-08-28 19:00:53 -0400

    added functions for input and display

--- Bmi/bmi_4cat.py
+++ Bmi/bmi_4cat.py
@@ -6,15 +6,29 @@ from gpiozero import LED
 # ToDo: fix issue with bad input - if anything is other than positive integers.
 # ToDo: the calculation of the BMI should be done as a function
 # ToDo: Change individual LEDs to  RGB LED
+# ToDo: Confirm the "Out of Scope" error from pylint is a PEP8 error
 
+# Function convert_lbs_to_kg uses standard conversion factor to take input as lbs and return value in kg.
 def convert_lbs_to_kg(lbs):
     return lbs*0.4536
 
+# Function convert_inches_to_meters uses standard conversion factor to take input as inches and return value in meters.
 def convert_inches_to_meters(inches):
     return inches/39.37
 
+# Function calculate_bmi uses standard formula to return bmi value from input values of kg and meters.
 def calculate_bmi(kg, meters):
     return kg / (meters**2)
+
+# Function get_int_from_user returns an entered value as integer and it should assign that value to
+# a variable corresponding to the prompt that is passed as an argument.
+def get_int_from_user(prompt):
+    return int(input(prompt))
+
+# Function display_message will display the message passed as an argument followed by a blank line.
+def display_message(message):
+    print(message)
+    return 1
 
 # Establish a pin connection for 3 LEDs (red 17, yellow 27, green 22
 #                                        and blue 23)
@@ -26,19 +40,17 @@ green_led = LED(22)     # Green LED used for Normal BMI category
 blue_led = LED(23)      # Blue LED used for Underweight category
 
 # Display welcome message for user.
-print()
-print("Welcome To The Python-based BMI calculator. ")
-print()
+display_message("Welcome To The Python-based BMI calculator. \n")
 
 # Get weight from user.
-lbs = int(input("Enter your weight in lbs as a whole number: "))
+lbs = get_int_from_user("Enter your weight in lbs as a whole number: ")
 
 # Convert weight from lbs to kg.
 # kg = lbs * 0.4536    (approximately)
 kg = convert_lbs_to_kg(lbs)
 
 # Get height in inches from user (will get converted later).
-inches = int(input("Enter your height in inches as a whole number: "))
+inches = get_int_from_user("Enter your height in inches as a whole number: ")
 
 # Convert height from inches to meters.
 meters = convert_inches_to_meters(inches)
@@ -50,13 +62,11 @@ bmi = calculate_bmi(kg, meters)
 #   Underweight and Obese should light the RED LED.
 #   Overweight should light the YELLOW LED.
 #   Normal weight should light the GREEN LED.
-print()
-print("Your BMI = %4.2f" %(bmi) + " kg/m2")
-print()
+display_message(("\nYour BMI = %4.2f" %(bmi) + " kg/m2\n"))
 
 # If bmi < 18.5
 if bmi < 18.5:
-    print("Underweight")
+    display_message("Underweight")
     blue_led.on()
     sleep(5)
 
